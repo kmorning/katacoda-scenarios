78bb6a87bbea5e756f6a56d223e14a532b4bee29
kmorning <kmorning@utilitieskingston.com>
2020-05-01 15:17:01 -0400

    turn off host key checking

--- kubernetes-scenario-dev/background.sh
+++ kubernetes-scenario-dev/background.sh
@@ -4,4 +4,5 @@ apt install -y ansible \
 && echo "[servers]" > /etc/ansible/hosts \
 && echo "host01 ansible_host=host01" >> /etc/ansible/hosts \
 && echo "node01 ansible_host=node01" >> /etc/ansible/hosts
+&& sed -i '/^\[defaults\]*/a host_key_checking = False' /etc/ansible/ansible.cfg
 echo "done" >> /opt/.backgroundfinished
