61db21dbe9ad7b3c39d062b347903dfe4f750284
kmorning <kmorning@utilitieskingston.com>
2020-05-01 14:35:21 -0400

    only install ansible on master

--- kubernetes-scenario-dev/background.sh
+++ kubernetes-scenario-dev/background.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+host=$(hostname)
 
 echo "Setting up ansible, please wait...."
 
-apt install -y ansible \
-&& echo "[servers]" > /etc/ansible/hosts \
-&& echo "" >> /etc/ansible/hosts \
-&& echo "" >> /etc/ansible/hosts \
-&& rm /etc/apt/sources.list.d/yarn.list*
+if [ ${host} == "master" ]; then
+    apt install -y ansible \
+    && echo "[servers]" > /etc/ansible/hosts \
+    && echo "host01 ansible_host=host01" >> /etc/ansible/hosts \
+    && echo "node01 ansible_host=node01" >> /etc/ansible/hosts
+fi
+
+rm /etc/apt/sources.list.d/yarn.list*
 
 echo "done" >> /opt/.backgroundfinished
