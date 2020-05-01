bb361d19cf55d4bb9c05e81535f2fc0938bbf6ea
kmorning <kmorning@utilitieskingston.com>
2020-05-01 14:56:29 -0400

    fix scripts

--- kubernetes-scenario-dev/background.sh
+++ kubernetes-scenario-dev/background.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-host=$(hostname)
 
-rm /etc/apt/sources.list.d/yarn.list*
-
-echo "Setting up ansible, please wait...."
-
-if [ ${host} == "master" ]; then
-    apt install -y ansible \
-    && echo "[servers]" > /etc/ansible/hosts \
-    && echo "host01 ansible_host=host01" >> /etc/ansible/hosts \
-    && echo "node01 ansible_host=node01" >> /etc/ansible/hosts
-    echo "done" >> /opt/.backgroundfinished
-fi
+apt install -y ansible \
+&& echo "[servers]" > /etc/ansible/hosts \
+&& echo "host01 ansible_host=host01" >> /etc/ansible/hosts \
+&& echo "node01 ansible_host=node01" >> /etc/ansible/hosts
+echo "done" >> /opt/.backgroundfinished
--- kubernetes-scenario-dev/foreground.sh
+++ kubernetes-scenario-dev/foreground.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 host=$(hostname)
 
+# Remove package source since it causes a GPG key expired warning, and breaks ansible apt update
+rm /etc/apt/sources.list.d/yarn.list*
+
+# Only Wait on master
 if [ ${host} == "master" ]; then
-    echo "Waiting to complete"
+    echo "Installing ansible please wait...."
     while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Done"
 fi
